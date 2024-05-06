a743be0253a4886d56f20a2ec2d9b5b26de6b234
Ted Cater <edward.cater@xdesign.com>
2024-05-06 15:32:34 +0100

    chore: fix tests, change font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex h-screen w-screen items-center justify-center">
-      <h1 className="flex flex-col gap-8 font-darumadrop text-9xl font-black leading-[0.64] text-gray-800">
+      <h1 className="flex flex-col gap-8 font-darumadrop text-8xl font-black leading-[0.64] text-gray-800">
         <div>ted</div>
         <div>etc</div>
       </h1>
